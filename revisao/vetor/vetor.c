2af0390bac348f3f9fdebbed3c34648181a03cbc
antoniooseas <antoniooseas@ufpi.edu.br>
2026-03-18 11:42:47 -0300

    REvisao de pointer

--- revisao/vetor/vetor.c
+++ revisao/vetor/vetor.c
@@ -27,7 +27,18 @@ float variancia(int n, float *vet, float media) {
 
 void print_vetor(int n, float *vet){	
 	for (int i = 0; i < n; i++)
-		printf("%.2f ", vet[i]);
+		printf("%.1f ", vet[i]);
+}
+
+void inverter_vetores(int n, float *vet){
+    for (int i = 0; i < n / 2; ++i)
+    {
+        float temp = vet[i];
+        // O elemento da frente recebe o de trás
+        vet[i] = vet[n - 1 - i];     
+        // O de trás recebe o que estava na frente, ito é, Salvo em temp
+        vet[n - 1 - i] = temp;       
+    }
 }
 
 
@@ -35,16 +46,20 @@ int main()
 {
 	srand(time(NULL)); // evitar numeros repetidos
 
-	float vet[100], m, v;
-	int n = 100;
+	float vet[10], m, v;
+	int n = 10;
 
 	capturar(n, vet);
 
 	m = media(n, vet);
 	v = variancia(n, vet, m);
 
+	printf("Vetor antes da troca \n");
 	print_vetor(n, vet);
-	printf("\nMedia: %.2f \nVariance: %.2f ", m, v);
+	// printf("\nMedia: %.2f \nVariance: %.2f ", m, v);
+	inverter_vetores(n, vet);
+	printf("\nVetor apos a troca \n");
+	print_vetor(n, vet);
 
 	return 0;
 }
